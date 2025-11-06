2bb3bb7e4bdcddb8541b503d13f32d58f9eb201d
Khang M N <192448446+mika0798@users.noreply.github.com>
2025-11-06 22:21:29 +0700

    Update Subqueries.sql

--- Subqueries.sql
+++ Subqueries.sql
@@ -12,7 +12,7 @@ SELECT name
 FROM students
 WHERE student_id IN (
 	SELECT student_id
-  FROM enrollments
+  	FROM enrollments
 );
 
 -- List students who are enrolled in both 'Introduction to Programming' and 'Calculus I' (2) using subquery + IN --
@@ -20,13 +20,13 @@ SELECT name AS Name, gpa AS GPA
 FROM students
 WHERE student_id IN (
 	SELECT student_id
-  FROM enrollments
-  WHERE course_name LIKE 'Introduction to Programming'
+  	FROM enrollments
+  	WHERE course_name LIKE 'Introduction to Programming'
 )
 AND student_id IN (
 	SELECT student_id
-  FROM enrollments
-  WHERE course_name LIKE 'Calculus I'
+  	FROM enrollments
+  	WHERE course_name LIKE 'Calculus I'
 );
 
 -- Find the students with the highest gpa of each course (1) using subquery + self-join --
@@ -36,8 +36,8 @@ JOIN enrollments e
 	ON s.student_id = e.student_id
 WHERE s.gpa = (
 	SELECT MAX(s2.gpa)
-  FROM students s2
-  JOIN enrollments e2
+  	FROM students s2
+  	JOIN enrollments e2
 	ON s2.student_id = e2.student_id
 	WHERE e.course_name = e2.course_name
 )
