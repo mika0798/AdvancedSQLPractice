b3c3c47da8a2be33cfda0a131a22da945551f353
Khang M N <192448446+mika0798@users.noreply.github.com>
2025-11-07 11:42:34 +0700

    Update ControlFlowProcedures.sql

--- ControlFlowProcedures.sql
+++ ControlFlowProcedures.sql
@@ -7,11 +7,11 @@ CREATE PROCEDURE checkGpa(IN student_id_prm INT, OUT status VARCHAR(20))
 BEGIN
 	DECLARE gpa_value DECIMAL (3,2);
     
-  SELECT gpa INTO gpa_value
-  FROM students
-  WHERE student_id = student_id_prm;
+  	SELECT gpa INTO gpa_value
+  	FROM students
+  	WHERE student_id = student_id_prm;
     
-  IF gpa_value >= 3.5 THEN
+  	IF gpa_value >= 3.5 THEN
 		SET status = 'Excellent';
 	ELSEIF gpa_value >= 2.0 THEN
 		SET status = 'Good';
