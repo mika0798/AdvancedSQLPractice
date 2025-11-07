e0fad87fbcf18722ecc4e1149dedcffa00f9e217
Khang M N <192448446+mika0798@users.noreply.github.com>
2025-11-07 11:48:42 +0700

    Update CTE.sql

--- CTE.sql
+++ CTE.sql
@@ -4,8 +4,8 @@
 -- Find majors with number of students less than 8 --
 WITH student_count AS (
 	SELECT major AS Major, COUNT(student_id) AS Student_Count
-  FROM students
-  GROUP BY major
+  	FROM students
+  	GROUP BY major
 ) 
 SELECT Major, Student_Count
 FROM student_count
